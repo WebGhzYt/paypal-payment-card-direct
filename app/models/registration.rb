0268b4a81ac2e5b58b9863258bb2affd14c40b5a
WebGhzYt <engrohitjain5@gmail.com>
2015-02-26 14:28:18 +0530

    Recurring with direct payment instllment

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -7,18 +7,48 @@ class Registration < ActiveRecord::Base
 
   serialize :notification_params, Hash
   def paypal_url(return_path,count)
+    #  For Non Recurring payment
+    # values = {
+    #     business: "engrohitjain5-facilitator@gmail.com",
+    #     cmd: "_xclick",
+    #     upload: 1,
+    #     return: "http://127.0.0.1:3000/",
+    #     invoice: count,
+    #     amount: course.price,
+    #     item_name: course.name,
+    #     item_number: course.id,
+    #     quantity: '1',
+    #     notify_url: "#{Rails.application.secrets.app_host}/hook"
+    # }
     values = {
         business: "engrohitjain5-facilitator@gmail.com",
-        cmd: "_xclick",
         upload: 1,
-        return: "http://127.0.0.1:3000/",
-        invoice: count,
-        amount: course.price,
-        item_name: course.name,
-        item_number: course.id,
-        quantity: '1',
-        notify_url: "#{Rails.application.secrets.app_host}/hook"
+        no_shipping: 1,
+        return: "#{Rails.application.secrets.app_host}#{return_path}",
+        notify_url: "#{Rails.application.secrets.app_host}/hook",
+        invoice: id,
+        item_name: course.name
     }
+
+    values = if course.recurring
+                values.merge(
+                    cmd: "_xclick-subscriptions",
+                    a3: course.price,
+                    p3: 1,
+                    srt: course.cycles,
+                    t3: course.period.first
+                )
+              else
+                values.merge(
+                    cmd: "_xclick",
+                    amount: course.price,
+                    item_number: course.id,
+                    quantity: '1'
+                )
+              end
+
+
+
     "#{Rails.application.secrets.paypal_host}/cgi-bin/webscr?" + values.to_query
   end
 
